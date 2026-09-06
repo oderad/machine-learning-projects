000522d93bc1e39863ba8d5b5ce1c6bd02293658
oderad <81644399+oderad@users.noreply.github.com>
2026-09-05 23:49:04 -0400

    Add files via upload

---
+++ Decision_Tree/DT_Split.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+scr = pd.read_csv(r'C:\Users\omard\Desktop\Educational__Information\Data_Science_Projects\Data_Analysis\Python\Machine_Learning\Decision_Tree\scores.csv')
+
+# Student Test scores
+scr['Pass_Measure'] = scr['Pass'].str.replace({'Yes':'1','No':'0'})
+scr['Pass_Measure'] = pd.to_numeric(scr['Pass_Measure'],errors='coerce')
+
+xs = scr[['StudyHours','Attendance']]
+ys = scr['Pass_Measure']
+
+# Split the data for training
+xs_test, xs_train, ys_test, ys_train = train_test_split(
+    xs,
+    ys,
+    test_size = 0.45,
+    random_state = 42
+)
+
+# Create and train the model
+model = DecisionTreeClassifier(random_state = 42)
+model.fit(xs_train, ys_train)
+
+# Predict the model on the unseen data
+# The xs_test, values the model did not see yet so here is where it will predict using the remaining x values
+# Note, these are the input x values to test
+predictions = model.predict(xs_test)
+
+# Accuracy
+# This compares the remaing y results with the remaining x results to view accuracy
+accuracy = accuracy_score(ys_test, predictions)
+
+print(accuracy)
